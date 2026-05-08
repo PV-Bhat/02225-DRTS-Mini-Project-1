b33955f3aafee06506213761f02e976628afac7d
Thoradur <91309831+Thoradur@users.noreply.github.com>
2026-05-08 20:58:36 +0200

    Fixed issues now it output data

--- miniproject2/parser.py
+++ miniproject2/parser.py
@@ -17,7 +17,7 @@ def parse_topology(file_path):
         links.append(Link(
             source=l['source'],
             destination=l['destination'],
-            delay=l['delay_ms'],
+            delay=l['delay'],
             bandwidth_mbps=l.get('bandwidth_mbps', bandwidth)
         ))
     return links
@@ -34,12 +34,12 @@ def parse_streams(file_path):
         streams.append(Stream(
             id=s['id'],
             name=s['name'],
-            pcp=s['pcp'],
-            size=s['size_bytes'],
-            period=s['period_ms'],
-            deadline=s['deadline_ms'],
+            pcp=s['PCP'],
+            size=s['size'],
+            period=s['period'],
+            deadline=s['destinations'][0]['deadline'],
             source=s['source'],
-            destination=s['destination']
+            destination=s['destinations'][0]['id']
         ))
     return streams
 
@@ -53,6 +53,6 @@ def parse_routes(file_path):
     for r in data['routes']:
         routes.append(Route(
             flow_id=r['flow_id'],
-            path=r['path']
+            path=[hop['node']for hop in r['paths'][0]]
         ))
     return routes
